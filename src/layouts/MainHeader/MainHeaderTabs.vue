313a3eec7f349fce31cab8c6481c27fb8448cbe9
gaoz <gaoz@sina.com>
2023-08-15 07:39:32 +0800

    feat: 完成标题行一级菜单组件的标识条自动调整大小功能

--- src/layouts/MainHeader/MainHeaderTabs.vue
+++ src/layouts/MainHeader/MainHeaderTabs.vue
@@ -1,8 +1,10 @@
 <!--  【平台标题栏】/【一级菜单 Tabs】  -->
 <script setup lang="ts">
-import {nextTick, onMounted, ref, watch} from 'vue'
+import {onMounted, ref, watch} from 'vue'
 import {useRoute} from 'vue-router'
 import {useRouter} from 'vue-router'
+
+import {useElementSize} from '@vueuse/core'
 
 const $route = useRoute()
 const $router = useRouter()
@@ -10,10 +12,10 @@ const $router = useRouter()
 
 // Tabs 数据
 const tabsData = [
-  {index: '0', label: '', icon: 'home', to: '/'},
-  {index: '1', label: '组件集', icon: 'extension', to: '/', size: '20px'},
-  {index: '2', label: '应用预览', icon: 'apps', to: '/'},
-  {index: '3', label: '应用开发', icon: 'code', to: '/'},
+  {index: '0', label: '', icon: 'home', to: '/', size: '22px'},  // 可自定义图标大小，默认 20px
+  {index: '1', label: '组件库', icon: 'extension', to: '/'},
+  {index: '2', label: '应用预览', icon: 'widgets', to: '/'},
+  {index: '3', label: '应用开发', icon: 'api', to: '/'},
 ]
 
 // 点击 Tab 时，更新激活项
@@ -26,32 +28,35 @@ const onTabClick = (index: string) => {
 
 // 激活项变化时，更改下方标识条的宽度和位置，并跳转到对应的路由
 const refTabWrapper = ref<HTMLElement | null>(null)
+const refTabs = ref([])
 
 onMounted(() => {
-  // 避免页面刚加载时，标识条的位置不正确（因图标字体未读取，宽度有误），设置 300ms 延迟
-  setTimeout(() => {
-    watch(activeIndex, (index) => {
-      const tab = tabsData.find(item => item.index === index)
-      
-      nextTick(() => {  // 等待 DOM 更新后，再获取元素位置
-        if (tab) {
-          const el = refTabWrapper.value?.querySelector('.active')
-          if (el) {
-            const wrapperLeft = refTabWrapper.value?.getBoundingClientRect().left
-            const left = el.getBoundingClientRect().left - wrapperLeft
-            const width = el.getBoundingClientRect().width
-            const indicator = refTabWrapper.value?.querySelector('.indicator') as HTMLElement
-            if (indicator) {
-              indicator.style.left = `${left}px`
-              indicator.style.width = `${width}px`
-            }
-          }
-          $router.push(tab.to)
+  watch(activeIndex, (index) => {
+    const tab = tabsData.find(item => item.index === index)
+    
+    if (tab) {
+      const el = refTabWrapper.value?.querySelector('.active') as HTMLElement
+      if (el) {
+        // 动态更新标识条的位置 left
+        const wrapperLeft = refTabWrapper.value?.getBoundingClientRect().left
+        const left = el.getBoundingClientRect().left - (wrapperLeft ?? 0)
+        const width = el.getBoundingClientRect().width
+        const indicator = refTabWrapper.value?.querySelector('.indicator') as HTMLElement
+        if (indicator) {
+          indicator.style.left = `${left}px`
+          
+          // 监听激活项的宽度，变化时动态更新标识条的宽度
+          const {width: activeWidth} = useElementSize(el)
+          const unWatch = watch(activeWidth, (width) => {
+            console.log('activeWidth', width + 32)
+            indicator.style.width = `${width + 32}px`  // 32px 为左右 padding 值
+          }, {immediate: true})
         }
-      })
-      
-    })
-  }, 300)
+      }
+      // 跳转到对应的路由
+      $router.push(tab.to)
+    }
+  }, {immediate: true, flush: 'post'})  // flush:post 等待 DOM 更新后，再执行 watch
 })
 
 
@@ -70,12 +75,12 @@ watch(() => $route.path, (path) => {
     
     <!--  Tabs 列表  -->
     <template v-for="item in tabsData" :key="item.label">
-      <div class="relative h-full px-4 flex items-center justify-center cursor-pointer
+      <div ref="refTabs" class="relative h-full px-4 flex items-center justify-center cursor-pointer
                   hover:bg-white/10"
            :class="{ 'active': activeIndex === item.index }"
            @click="onTabClick(item.index)"
       >
-        <EIcon :name="item.icon" :size="item.size ?? '22px'" class="text-xl"
+        <EIcon :name="item.icon" :size="item.size ?? '20px'" class="text-xl"
                :class="{ 'mr-2': item.label.length > 0 }"/>
         <span class="text-sm">
           {{ item.label }}
@@ -84,7 +89,7 @@ watch(() => $route.path, (path) => {
     </template>
     
     <!--  激活项下方标识条  -->
-    <div class="indicator absolute bottom-px left-0 w-[54px] h-0.5 bg-header duration-300"/>
+    <div class="indicator absolute bottom-px left-0 w-[54px] h-0.5 bg-header duration-300 ease-in-out"/>
   </div>
 </template>
 
